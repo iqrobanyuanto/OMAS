df1e35af926a02d8bf0d88899cc586e4fd68cb19
iqrobanyuanto <iqrobanyuanto12345@gmail.com>
2023-04-12 23:53:02 +0700

    Intial commit

---
+++ src/main/java/com/mycompany/omas/OMAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.omas;
+
+/**
+ *
+ * @author Iqro Banyuanto
+ */
+public class OMAS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
