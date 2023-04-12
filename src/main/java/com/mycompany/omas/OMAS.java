462c25325af14cc82b14bec11d93738810d916dc
iqrobanyuanto <iqrobanyuanto12345@gmail.com>
2023-04-12 23:57:01 +0700

    tambah hello world

--- src/main/java/com/mycompany/omas/OMAS.java
+++ src/main/java/com/mycompany/omas/OMAS.java
@@ -12,5 +12,8 @@ public class OMAS {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
